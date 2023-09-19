e9f6c41ede754570f6cd241acff597427d3eb1c5
Muhammad Adeel <m7adeel@gmail.com>
2023-09-19 21:49:13 +0500

    fixed bugs in return statement of the flask app routes

--- main.py
+++ main.py
@@ -2,43 +2,47 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#addition function
+# Addition function
+def add(a, b):
+    return a + b
 
-def add(a,b):
-	return a+b
-	
-#subtraction function
+# Subtraction function
+def subtract(a, b):
+    return a - b
 
-def subtract(a,b):
-	return a-b
-
+# Multiplication function
 def multiply(num1, num2):
-    return (num1*num2)
+    return num1 * num2
 
+# Division function
 def divide(num1, num2):
-    return (num1/num2)
+    if num2 == 0:
+        return "Error: Division by zero"
+    return num1 / num2
+
+# Modulus function
+def mod(num1, num2):
+    return num1 % num2
 
 @app.route('/add/<int:num1>/<int:num2>', methods=['GET'])
-def addRoute():
-    return f'{add(num1, num2)}'
+def addRoute(num1, num2):
+    return str(add(num1, num2))
 
 @app.route('/subtract/<int:num1>/<int:num2>')
-def subtractRoute():
-    return subtract(num1, num2)
+def subtractRoute(num1, num2):
+    return str(subtract(num1, num2))
 
 @app.route('/multiply/<int:num1>/<int:num2>')
-def multiplyRoute():
-    return multiply(num1, num2)
+def multiplyRoute(num1, num2):
+    return str(multiply(num1, num2))
 
 @app.route('/divide/<int:num1>/<int:num2>')
-def divideRoute():
-    return divide(num1, num2)
+def divideRoute(num1, num2):
+    return str(divide(num1, num2))
 
 @app.route('/mod/<int:num1>/<int:num2>')
-def mod():
-    return mod(num1,num2)
+def modRoute(num1, num2):
+    return str(mod(num1, num2))
 
-    
-  
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
